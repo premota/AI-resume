03075c49a6393215a1066950fde95e101fb134e3
asemota precious <precious.o.asemota@gmail.com>
2026-05-26 02:12:26 +0100

    included logger for observability

--- src/AgentFramework/AgentFactory/agent.py
+++ src/AgentFramework/AgentFactory/agent.py
@@ -1,4 +1,5 @@
 from typing import Generic
+import logging
 
 from pydantic_ai import Agent as PydanticAIAgent
 from AgentFramework.AgentFactory.agent_config import (
@@ -9,6 +10,7 @@ from AgentFramework.AgentFactory.agent_config import (
 from AgentFramework.AgentFactory.tool_registry import ToolRegistry
 from utils.exception import AgentInitializationError, ToolRegistryError
 
+logger = logging.getLogger(__name__)
 
 class Agent(Generic[AgentDepsT, AgentOutputT]):
     def __init__(self, config: AgentConfig):
@@ -22,6 +24,7 @@ class Agent(Generic[AgentDepsT, AgentOutputT]):
     def _instantiate_agent(self) -> PydanticAIAgent[AgentDepsT, AgentOutputT]:
         if self._agent is not None:
             return self._agent
+        logger.info("Instantiating pydantic Agent with model config: %s", self.config)
         try:
             temp_agent = PydanticAIAgent(
                 model=self.config.model,
@@ -30,19 +33,23 @@ class Agent(Generic[AgentDepsT, AgentOutputT]):
                 deps_type=self.config.dep_types,
             )
         except Exception as e:
+            logger.exception("Agent initialization failed")
             raise AgentInitializationError("Agent failed to initialize") from e
 
         # register tools to agent
         self._register_tools(temp_agent)
-
+        logger.info("Agent initialization complete")
         self._agent = temp_agent
         return self._agent
 
     def _register_tools(self, agent: PydanticAIAgent) -> None:
         try:
+            logger.info("Registering tools %s", self.config.tool)
             ToolRegistry(agent=agent, tool_list=self.config.tool).register_tools()
         except Exception as e:
+            logger.exception("Tool registration failed")
             raise ToolRegistryError("Tools failed to register") from e
+        logger.info("Tool registration complete")
 
     def get_agent(self) -> PydanticAIAgent[AgentDepsT, AgentOutputT]:
         if self._agent is None:
